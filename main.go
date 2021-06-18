32358882bcafb5897c7fd711493799f5edfc7a6a
ward <850781645@qq.com>
2021-06-18 23:34:10 +0800

    测试

--- main.go
+++ main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"fmt"
-	"gin_one/app/blog"
-	"gin_one/app/shop"
-	"gin_one/app/login"
 	"gin_one/app/appall"
+	"gin_one/app/blog"
+	"gin_one/app/login"
+	"gin_one/app/shop"
 	"gin_one/routers"
 )
 
 func main() {
-	// 
+	// ceshi
 	// 加载多个APP的路由配置
 	routers.Include(shop.Routers, blog.Routers, login.Routers, appall.Routers)
 	// 初始化路由
 	r := routers.Init()
 	if err := r.Run(":8090"); err != nil {
-	fmt.Println("startup service failed, err:%v\n", err)
+		fmt.Println("startup service failed, err:%v\n", err)
 	}
 }
